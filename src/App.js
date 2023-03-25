22c47deabd07e445911f099af6da0f19ac51e5b4
jaimesi <si.ja@northeastern.edu>
2023-03-25 13:12:45 -0400

    a7 2.2

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
                          element={<Labs/>}/>
                   <Route path="/hello" element={<HelloWorld/>}/>
                   <Route path="/tuiter/*" element={<Tuiter/>}/>
-
+                  <Route path="/*" element={<Labs/>}/>
               </Routes>
           </div>
       </BrowserRouter>
